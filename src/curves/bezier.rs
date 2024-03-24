c0897399213c1c0229ccdab60975283d863a06a2
gnawinganimal <unrongeur@proton.me>
2024-03-24 13:40:34 -0700

    Hermite to bezier conversions

--- src/curves/bezier.rs
+++ src/curves/bezier.rs
@@ -1,5 +1,7 @@
 use crate::{curves::Curve, vec::Vec2};
 
+use super::hermite::Hermite;
+
 pub struct Bezier { 
     pub p0: Vec2,
     pub p1: Vec2,
@@ -16,10 +18,23 @@ impl Curve for Bezier {
     }
 }
 
+impl From<&Hermite> for Bezier {
+    fn from(c: &Hermite) -> Self {
+        Self {
+            p0: c.p0,
+            p1: c.p0 + (c.v0 / 3.),
+            p2: c.p1 - (c.v1 / 3.),
+            p3: c.p1,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{curves::Curve, vec::Vec2};
+    use crate::{curves::{hermite::Hermite, Curve}, vec::Vec2};
     use super::Bezier;
+
+    use approx::assert_ulps_eq;
 
     #[test]
     pub fn endpoints_are_correct() {
@@ -32,5 +47,39 @@ mod tests {
 
         assert_eq!(c.get(0.0), c.p0);
         assert_eq!(c.get(1.0), c.p3);
+    }
+
+    #[test]
+    pub fn converts_endpoints_from_hermite() {
+        let h = Hermite {
+            p0: Vec2(0.0, 0.0),
+            p1: Vec2(4.0, 1.0),
+            v0: Vec2(1.0, 1.0),
+            v1: Vec2(1.0, -1.0)
+        };
+
+        let b = Bezier::from(&h);
+        
+        assert_eq!(h.get(0.), b.get(0.));
+        assert_eq!(h.get(1.), b.get(1.)); 
+    }
+
+    #[test]
+    pub fn converts_hermite_to_bezier() {
+        let h = Hermite {
+            p0: Vec2(0.0, 0.0),
+            p1: Vec2(4.0, 1.0),
+            v0: Vec2(1.0, 1.0),
+            v1: Vec2(1.0, -1.0)
+        };
+
+        let b = Bezier::from(&h);
+
+        assert_ulps_eq!(h.get(0.0), b.get(0.0));
+        assert_ulps_eq!(h.get(0.2), b.get(0.2));
+        assert_ulps_eq!(h.get(0.4), b.get(0.4));
+        assert_ulps_eq!(h.get(0.6), b.get(0.6));
+        assert_ulps_eq!(h.get(0.8), b.get(0.8));
+        assert_ulps_eq!(h.get(1.0), b.get(1.0));
     }
 }
--- src/vec.rs
+++ src/vec.rs
@@ -1,4 +1,5 @@
 use std::ops::{Add, Div, Mul, Neg, Sub};
+use approx::{AbsDiffEq, RelativeEq, UlpsEq};
 
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub struct Vec2(pub f32, pub f32);
@@ -78,6 +79,41 @@ impl Sub<Vec2> for Vec2 {
 
     fn sub(self, v: Vec2) -> Self::Output {
         Self(self.0 - v.0, self.1 - v.1)
+    }
+}
+
+impl AbsDiffEq for Vec2 {
+    type Epsilon = <f32 as AbsDiffEq>::Epsilon;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f32::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        f32::abs_diff_eq(&self.0, &other.0, epsilon) &&
+        f32::abs_diff_eq(&self.1, &other.1, epsilon)
+    }
+}
+
+impl RelativeEq for Vec2 {
+    fn default_max_relative() -> Self::Epsilon {
+        f32::default_max_relative()
+    }
+
+    fn relative_eq(&self, other: &Self, epsilon: Self::Epsilon, max_relative: Self::Epsilon) -> bool {
+        f32::relative_eq(&self.0, &other.0, epsilon, max_relative) &&
+        f32::relative_eq(&self.1, &other.1, epsilon, max_relative)
+    }
+}
+
+impl UlpsEq for Vec2 {
+    fn default_max_ulps() -> u32 {
+        f32::default_max_ulps()
+    }
+
+    fn ulps_eq(&self, other: &Self, epsilon: Self::Epsilon, max_ulps: u32) -> bool {
+        f32::ulps_eq(&self.0, &other.0, epsilon, max_ulps) &&
+        f32::ulps_eq(&self.1, &other.1, epsilon, max_ulps)
     }
 }
 
